459bd9b25306f280b261d05cc92ab7147b2bb1c2
Duduf <git@duduf.com>
2019-10-08 20:09:58 +0200

    new expressions (transform/parent constraint)

---
+++ src/libs/expressionLib/getOrientationAtTime.js
@@ -0,0 +1,18 @@
+/**
+ * Gets the world orientation of a (2D) layer at a specific time.
+ * @function
+ * @name getOrientationAtTime
+ * @param {Layer} l The layer to get the orientation from
+ * @param {float} [t=time] The time at which to get the orientation
+ * @return {float} The orientation, in degrees.
+ */
+function getOrientationAtTime( l, t ) {
+    if (typeof t === "undefined" ) t = time;
+    var r = 0;
+    r += l.rotation.valueAtTime( t );
+    while ( l.hasParent ) {
+        l = l.parent;
+        r += l.rotation.valueAtTime( t );
+    }
+    return r;
+}
---
+++ src/libs/expressionLib/translatePointWithLayer.js
@@ -0,0 +1,24 @@
+/**
+ * Translates a point with a layer, as if it was parented to it.
+ * @function
+ * @name translatePointWithLayer
+ * @param {Layer} l The layer to get the translation from.
+ * @param {float[]} [point=[0,0]] The [X,Y] point to translate (using world coordinates).
+ * @param {float} [startFrame=0] The start frame of the translation
+ * @param {float} [endFrame=timeToFrames()] The end frame of the translation
+ * @return {float[]} The coordinates of the translated point.
+ */
+function translatePointWithLayer( l, point, startFrame, endFrame ) {
+    if (typeof startFrame === "undefined") startFrame = 0;
+    if (typeof endFrame === "undefined") endFrame = timeToFrames();
+    var sT = framesToTime( startFrame );
+    var eT = framesToTime( endFrame );
+    try {
+        var pos = l.fromWorld( point, sT );
+    } catch ( e ) {
+        var pos = [ 0, 0 ];
+    }
+    var prevPos = l.toWorld( pos, sT );
+    var newPos = l.toWorld( pos, eT );
+    return newPos - prevPos;
+}
