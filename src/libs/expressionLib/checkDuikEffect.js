3f834fa9aa8928411b009918ab3e123ecad3a140
Duduf <git@duduf.com>
2019-10-08 18:19:55 +0200

    Create checkDuikEffect.js

---
+++ src/libs/expressionLib/checkDuikEffect.js
@@ -0,0 +1,22 @@
+/**
+ * Checks the type of a pseudo-effect used by Duik.<br />
+ * This is a workaround for the missing matchName in expressions.<br />
+ * Pseudo-Effects used by Duik start with a hidden property which name is the same as the matchName of the effect itself (without the 'Pseudo/' part).
+ * @example
+ * if ( checkEffect(thisLayer.effect(1), "DUIK parentConstraint2") ) { "This is the second version of the parent constraint by Duik" }
+ * else { "Who knows what this is?" }
+ * @param {Property} fx The effect to check
+ * @param {string} duikMatchName The matchName of a pseudo-effect used by Duik (without the 'Pseudo/' part)
+ * @return {boolean} True when the property at propIndex is named propName
+ */
+function checkDuikEffect(fx, duikMatchName) {
+    if (fx.numProperties  < 1) return false;
+    if (!!$.engineName) {
+        if ( fx(1).name != duikMatchName ) return false;
+    }
+    else {
+        try { if (fx(1).name != duikMatchName) return false; }
+        catch (e) { return false; }
+    }
+    return true;
+}
