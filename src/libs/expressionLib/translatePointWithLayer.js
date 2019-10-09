a74de7fadaaca0f914798bff8d396d7abc14e5da
Duduf <git@duduf.com>
2019-10-09 17:37:56 +0200

    fix DuAEF_DUIK#520

--- src/libs/expressionLib/translatePointWithLayer.js
+++ src/libs/expressionLib/translatePointWithLayer.js
@@ -3,21 +3,19 @@
  * @function
  * @param {Layer} l The layer to get the translation from.
  * @param {float[]} [point=[0,0]] The [X,Y] point to translate (using world coordinates).
- * @param {float} [startFrame=0] The start frame of the translation
- * @param {float} [endFrame=timeToFrames()] The end frame of the translation
+ * @param {float} [startT=0] The start time of the translation
+ * @param {float} [endT=timeToFrames()] The end time of the translation
  * @return {float[]} The coordinates of the translated point.
  */
-function translatePointWithLayer( l, point, startFrame, endFrame ) {
-    if (typeof startFrame === "undefined") startFrame = 0;
-    if (typeof endFrame === "undefined") endFrame = timeToFrames();
-    var sT = framesToTime( startFrame );
-    var eT = framesToTime( endFrame );
+function translatePointWithLayer( l, point, startT, endT ) {
+    if (typeof startT === "undefined") startT = 0;
+    if (typeof endT === "undefined") endT = time;
     try {
-        var pos = l.fromWorld( point, sT );
+        var pos = l.fromWorld( point, startT );
     } catch ( e ) {
         var pos = [ 0, 0 ];
     }
-    var prevPos = l.toWorld( pos, sT );
-    var newPos = l.toWorld( pos, eT );
+    var prevPos = l.toWorld( pos, startT );
+    var newPos = l.toWorld( pos, endT );
     return newPos - prevPos;
 }
