00e52da578f6a8c56692488fb960303bdb6b0af2
Nicolas Dufresne <git@duduf.com>
2019-11-03 11:56:47 +0100

    Update translatePointWithLayer.js

--- src/libs/expressionLib/translatePointWithLayer.js
+++ src/libs/expressionLib/translatePointWithLayer.js
@@ -4,7 +4,7 @@
  * @param {Layer} l The layer to get the translation from.
  * @param {float[]} [point=[0,0]] The [X,Y] point to translate (using world coordinates).
  * @param {float} [startT=0] The start time of the translation
- * @param {float} [endT=timeToFrames()] The end time of the translation
+ * @param {float} [endT=time] The end time of the translation
  * @return {float[]} The coordinates of the translated point.
  */
 function translatePointWithLayer( l, point, startT, endT ) {
