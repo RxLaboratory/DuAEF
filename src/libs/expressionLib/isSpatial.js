9f70fb3f8fa3044e39db60b87805a9423a5a71cf
Duduf <git@duduf.com>
2019-11-07 19:25:02 +0100

    Create isSpatial.js

---
+++ src/libs/expressionLib/isSpatial.js
@@ -0,0 +1,16 @@
+/**
+ * Checks if a property is spatial
+ * @param {Property} prop The property to check
+ * @return {boolean} true if the property is spatial.
+ */
+function isSpatial(prop) {
+	if (typeof prop === 'undefined') prop = thisProperty;
+	if (!(prop.value instanceof Array)) return false;
+	if (prop.value.length != 2 && prop.length != 3) return false;
+	try {
+		sp = prop.speed;
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
