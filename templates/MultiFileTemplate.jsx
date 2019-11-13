d72ce6cf66b1c368ae6ff4a1bf006d86f8cd7567
Duduf <duduf@duduf.com>
2019-11-13 16:42:35 +0100

    updated templates

---
+++ templates/MultiFileTemplate.jsx
@@ -0,0 +1,5 @@
+#include 'multiFileTemplate_required/MultiFileTemplate_header.jsxinc'
+
+DuAEF.DuScriptUI.addStaticText(contents, "Welcome to " + DuAEF.scriptName);
+
+#include 'multiFileTemplate_required/MultiFileTemplate_footer.jsxinc'
--- templates/SingleFileTemplate.jsx
+++ templates/SingleFileTemplate.jsx
@@ -7,10 +7,8 @@ The script can be launched both from ScriptUI Panels or the File/Scripts/Run Scr
 (function (thisObj) {
 
 	//================
-	var version = '0.0.0';
-	var scriptName = "The Script Name";
-	//================
 	#include DuAEF.jsxinc
+	DuAEF.init("Your Script Name", "1.0.2");
 
 	// ================ FUNCTIONS =============
 	//MAIN
@@ -20,12 +18,14 @@ The script can be launched both from ScriptUI Panels or the File/Scripts/Run Scr
 
 
 	// _______ UI SETUP _______
-	var ui = DuAEF.DuScriptUI.createUI(thisObj,scriptName);
+	var ui = DuAEF.DuScriptUI.createUI(thisObj);
 
 	// ============ UI CONTENT =================
+	var contents = ui.contents;
+	DuAEF.DuScriptUI.addStaticText(contents, "Welcome to " + DuAEF.scriptName);
 
 
 	//Show UI
 	DuAEF.DuScriptUI.showUI(ui);
-
+	DuAEF.enterRunTime();
 })(this);
