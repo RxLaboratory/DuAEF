5caa234ed6ca690afce65ef5f147c99357efd23c
Duduf <git@duduf.com>
2019-11-23 19:20:14 +0100

    updated template

--- templates/SingleFileTemplate.jsx
+++ templates/SingleFileTemplate.jsx
@@ -10,6 +10,15 @@ The script can be launched both from ScriptUI Panels or the File/Scripts/Run Scr
 	#include DuAEF.jsxinc
 	DuAEF.init("Your Script Name", "1.0.2");
 
+	DuAEF.forumURL = 'https://forum.rainboxlab.org';
+    DuAEF.chatURL = 'http://chat.rainboxlab.org';
+    DuAEF.bugReportURL = 'https://github.com/Rainbox-dev/DuAEF_Duik/issues/new?template=bug_report.md';
+    DuAEF.featureRequestURL = 'https://github.com/Rainbox-dev/DuAEF_Duik/issues/new?template=feature_request.md';
+    DuAEF.aboutURL = 'https://rainboxlab.org/tools/duik/';
+    DuAEF.scriptAbout = 'The Duduf After Effects Framework\nDeveloped by Nicolas Dufresne and Contributors.\n\nLicensed under the GNU General Public License v3.';
+    DuAEF.newsServer = 'rainboxprod.net'
+	DuAEF.newsArgs = 'wp/?call_custom_simple_rss=1&csrp_show_meta=0&csrp_cat=20';
+
 	// ================ FUNCTIONS =============
 	//MAIN
 
