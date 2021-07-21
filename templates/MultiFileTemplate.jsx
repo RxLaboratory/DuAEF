c1522c86c34b3b73e713979c55f6972a530a54ad
Duduf <git@duduf.com>
2021-07-21 17:18:04 +0200

    reorganized repo

--- templates/MultiFileTemplate.jsx
+++
@@ -1,5 +0,0 @@
-#include 'multiFileTemplate_required/MultiFileTemplate_header.jsxinc'
-
-DuScriptUI.staticText(ui.contents, "Welcome to " + DuESF.scriptName);
-
-#include 'multiFileTemplate_required/MultiFileTemplate_footer.jsxinc'
