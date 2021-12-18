47a2c78244cd1f611ee86150997d47ab1077c917
Duduf <git@duduf.com>
2021-12-18 11:04:26 +0100

    fixed cleanKeyframes & beautify

--- test.jsx
+++ test.jsx
@@ -7,9 +7,13 @@
     DuAEF.init("DuAEF Test", "0.0.0", "RxLaboratory");
     DuAEF.enterRunTime();
 
+    DuAE.beginUndoGroup("DuAEF Test");
+
     var props = DuAEComp.getSelectedProps();
     props = new DuList(props);
     props.do(function(prop) {
-        prop.quickBakeExpression(.25);
+        prop.quickBakeExpression(4);
     });
+
+    DuAE.endUndoGroup();
 })(this);
