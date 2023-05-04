6e183c70103cd46de87a8a6afecd94d97f33a0bb
Duduf <git@duduf.com>
2023-05-04 21:03:32 +0200

    trying to fix the valueAtTime timing issue

--- test.jsx
+++ test.jsx
@@ -3,8 +3,5 @@
 //encapsulate the script in a function to avoid global variables
 (function (thisObj) {
 
-    #include "DuAEF.jsxinc"
-
-    alert( DuAELayer.isRenderable( app.project.activeItem.layer(1) ) );
 
 })(this);
